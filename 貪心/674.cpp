05687347c8aaea3d65d973ef7e60daf0411e6762
mizunokaze <mizunokaze114514@gmail.com>
2026-01-18 22:04:02 +0800

    Create 674.cpp

---
+++ 貪心/674.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findLengthOfLCIS(vector<int>& nums) {
+        int n = nums.size();
+        if(n <= 1) return n;
+        int ans = 1, cnt = 1;
+
+        for(int i = 0; i < n - 1; i ++){
+            if(nums[i + 1] > nums[i]) cnt ++;
+            else cnt = 1;
+            ans = max(ans, cnt);
+        }
+        return ans;
+    }
+};
