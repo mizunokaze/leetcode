7aaf756ffb60109225b63680e5a6340a3aa9c994
mizunokaze <mizunokaze114514@gmail.com>
2026-01-20 14:03:43 +0800

    Create 134.cpp

---
+++ 貪心/遍歷/134.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int ans = 0, min_s = 0, s = 0;
+        for(int i = 0; i < gas.size(); i ++){
+            s += gas[i] - cost[i];
+            if(s < min_s){
+                min_s = s, ans = i + 1;
+            }
+        }
+        return s < 0 ? -1 : ans;
+    }
+};
